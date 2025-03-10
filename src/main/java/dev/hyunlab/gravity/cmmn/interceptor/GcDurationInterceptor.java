ef289b2ce264e157947f75ac1bf74a6c75b6560e
gravity@vaiv.kr <gravity@vaiv.kr>
2025-03-10 16:46:44 +0900

    log.debug => log.info

--- src/main/java/dev/hyunlab/gravity/cmmn/interceptor/GcDurationInterceptor.java
+++ src/main/java/dev/hyunlab/gravity/cmmn/interceptor/GcDurationInterceptor.java
@@ -2,11 +2,13 @@ package dev.hyunlab.gravity.cmmn.interceptor;
 
 import java.util.Date;
 
+import org.springframework.lang.NonNull;
 import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
+import jakarta.annotation.Nonnull;
 import jakarta.annotation.PostConstruct;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -22,28 +24,31 @@ public class GcDurationInterceptor implements HandlerInterceptor {
     }
 
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+    public boolean preHandle(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response,
+            @NonNull Object handler)
             throws Exception {
 
         request.setAttribute("startDt", new Date());
 
         // TODO 업무로직
 
-        log.debug("<< {}", request.getRequestURI());
+        log.info("<< {}", request.getRequestURI());
         return true;
     }
 
     @Override
-    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+    public void postHandle(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response,
+            @NonNull Object handler,
             @Nullable ModelAndView modelAndView) throws Exception {
 
         // 소요시간
-        log.debug("<< DURATION:{}ms {}", (new Date().getTime() - ((Date) request.getAttribute("startDt")).getTime()),
+        log.info("<< DURATION:{}ms {}", (new Date().getTime() - ((Date) request.getAttribute("startDt")).getTime()),
                 request.getRequestURI());
     }
 
     @Override
-    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler,
+    public void afterCompletion(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response,
+            @NonNull Object handler,
             @Nullable Exception ex) throws Exception {
 
         log.debug("<<");
